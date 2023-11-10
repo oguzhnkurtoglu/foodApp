3d680e9acf3caec6bc024ca812fc72e1e04a2d66
= <=>
2023-11-10 03:22:44 +0300

    unutulan console silindi

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HeadlineCards from "./components/HeadlineCards";
-import Food from "./components/Food";
-import Category from "./components/Category";
-import { useState } from "react";
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import HeadlineCards from "./components/HeadlineCards"
+import Food from "./components/Food"
+import Category from "./components/Category"
+import { useState } from "react"
 
 function App() {
-  const [input, setInput] = useState("");
-  console.log(input);
+	const [input, setInput] = useState("")
 
-  return (
-    <>
-      <Navbar setInput={setInput} />
-      <Hero />
-      <HeadlineCards />
-      <Food input={input} />
-      <Category />
-    </>
-  );
+	return (
+		<>
+			<Navbar setInput={setInput} />
+			<Hero />
+			<HeadlineCards />
+			<Food input={input} />
+			<Category />
+		</>
+	)
 }
 
-export default App;
+export default App
